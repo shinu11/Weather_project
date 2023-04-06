558413f3e05d774ed9b8472b6c48770fc554c7fb
Shinu kp <shinunandu123@gmail.com>
2023-04-06 11:20:51 +0530

    model created for weather

--- weather_app/models.py
+++ weather_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Weather(models.Model):
+    location = models.CharField(max_length=100)
+    temperature = models.DecimalField(max_digits=5,decimal_places=2)
+    humidity = models.DecimalField(max_digits=5,decimal_places=2)
+    wind_speed = models.DecimalField(max_digits=5, decimal_places=2)
